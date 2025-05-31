d67ee6fa7f1fc22ba624da893c5790fc4134c84b
frodx <edsondias30@gmail.com>
2025-05-31 02:50:40 -0300

    Update api_licenca.py

--- api_licenca.py
+++ api_licenca.py
@@ -63,4 +63,7 @@ def validar():
     return jsonify({"status": "invalido"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
